d3f6af710160f93585f3528a08b945c56d75945b
jjingohenry-ops <jjingohenry@gmail.com>
2026-04-03 00:12:03 +0300

    Fix Flutter web white screen by resetting stale caches

--- frontend/web/flutter_bootstrap.js
+++ frontend/web/flutter_bootstrap.js
@@ -1,8 +1,22 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: {{flutter_service_worker_version}},
-  },
-});
+// Force fresh app boot on deployments where stale service-worker caches can
+// cause a white screen due to old asset manifests.
+(async function bootstrapFlutter() {
+  if (typeof window !== 'undefined' && 'caches' in window) {
+    const cacheKeys = await caches.keys();
+    await Promise.all(cacheKeys.map((key) => caches.delete(key)));
+  }
+
+  if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.map((registration) => registration.unregister()));
+  }
+
+  _flutter.loader.load({
+    serviceWorkerSettings: {
+      serviceWorkerVersion: null,
+    },
+  });
+})();
